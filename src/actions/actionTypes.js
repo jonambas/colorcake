99ba69ca0987e7c9477b05acf8faf8aa051c2370
Jon Ambas <jon@jonambas.com>
2017-06-02 15:29:59 -0400

    Updates

--- src/actions/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const SET_BG = 'SET_BG';
-export const SET_FG = 'SET_FG';
-export const REVERSE = 'REVERSE';
-export const TEST = 'TEST';
--- src/store/initialState.js
+++
@@ -1,6 +0,0 @@
-export default {
-  color: {
-    background: '#2C44F1',
-    foreground: '#ffffff',
-  },
-};
